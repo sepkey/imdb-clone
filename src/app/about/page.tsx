6cb8933a97e2fd01c97f79a36d22f765fae00c95
Sepide Kia <sepidekia6990@gmail.com>
2023-11-16 10:14:43 +0330

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-teal-500">About us</h1>
+      <p>
+        Welcome to our website, a go-to destination for all your entertainment
+        needs! If you&apos;re a movie and TV enthusiast, then you&apos; ve come
+        to the right place. Our platform is dedicated to providing you with the
+        latest news, reviews, trailers, and much more. Think of us as your
+        personal guide to the world of entertainment.
+      </p>
+      <p>
+        With a vast collection of data and information on movies, TV shows, and
+        celebrities, our website is like the IMDb (Internet Movie Database) of
+        the internet. Whether you&apos;re looking for information on your
+        favorite actors, the latest blockbuster movies, or indie gems,
+        we&apos;ve got you covered.
+      </p>
+      <p>
+        We strive to curate the best content for our users and present it in an
+        easy-to-navigate format. So, sit back, relax, and explore all that we
+        have to offer. Whether you&apos;re a casual viewer or a hardcore movie
+        and TV buff, there&apos;s something for everyone here. Thanks for
+        visiting, and we hope you enjoy the ride!
+      </p>
+    </div>
+  );
+}
