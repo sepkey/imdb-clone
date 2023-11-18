32fe75f4c4abe8b9090284a91f05f7877961a2d3
Sepide Kia <sepidekia6990@gmail.com>
2023-11-18 23:31:40 +0330

    change into img element

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-96" src="spinner.svg" alt="loading..." />
+      <img className="h-96" src="spinner.svg" alt="loading..." />
     </div>
   );
 }
